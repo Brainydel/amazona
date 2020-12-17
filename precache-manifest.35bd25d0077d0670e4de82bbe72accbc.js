96b91810def0a061a1f2b1a81c32f77dd5df26d0
Brainydel <71158893+Brainydel@users.noreply.github.com>
2020-12-17 23:17:11 +0100

    Add files via upload

---
+++ precache-manifest.35bd25d0077d0670e4de82bbe72accbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972f5c879a8339c4d906e45c632de56a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26366a29220faa1378a3",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "b23497a08d62cb7daca6",
+    "url": "/static/css/main.00db0710.chunk.css"
+  },
+  {
+    "revision": "26366a29220faa1378a3",
+    "url": "/static/js/2.fe333fc7.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/static/js/2.fe333fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23497a08d62cb7daca6",
+    "url": "/static/js/main.dfae9b0a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
